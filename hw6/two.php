bd9680ebacfdc61fd406491a7c3dda0dd9b3e7d3
Unknown <supawadee.ri@ku.th>
2017-04-11 21:10:25 +0700

    hw6

---
+++ hw6/two.php
@@ -0,0 +1,50 @@
+<html>
+<body>
+<h2>21.รหัสลูกค้า ชื่อและนามสกุลลูกค้า ว่าไปดูอสังหาวันไหนบ้าง</h2>
+<?php
+echo "<table style='border: solid 1px black;'>";
+echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator {
+    function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+
+    function current() {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() {
+        echo "<tr>";
+    }
+
+    function endChildren() {
+        echo "</tr>" . "\n";
+    }
+}
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "dreamhome";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT client.fname, client.lname,viewing.viewdate FROM client INNER JOIN viewing ON (client.clientno = viewing.clientno)");
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+        echo $v;
+    }
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+?>
+</body>
+</html>
